0b77b6aeab2eb6e434674e54ca91b2e06529eb77
yokikoshimizu <heppoko.yokiki@gmail.com>
2021-12-09 13:34:45 +0900

    .

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const buttonAlert = () => {
+    window.alert('警告ざます')
+  }
   return (
     <div className="App">
-      <button type="button" onClick="hello()">Hello World</button>
-      <script>
-        function hello() {
-          window.alert('警告ざます')
-        }
-      </script>
+      <button onClick={buttonAlert}>Hello World</button>
     </div>
-  )
+  );
 }
 
 export default App;
