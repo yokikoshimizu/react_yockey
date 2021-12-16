314d797b13d315a296958046c541037c319a31e1
yokikoshimizu <heppoko.yokiki@gmail.com>
2021-12-16 13:55:47 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { render } from '@testing-library/react';
 
 function App() {
+  const [count, setCount ] = useState(0);
+  const name = 'yokiko';
   const buttonAlert = () => {
     window.alert('警告ざます')
   }
+
+    const increment = () => {
+      setCount(count + 1);
+      console.log(count);
+    }
+    const decrement = () => {
+      setCount(count - 1);
+      console.log(count);
+    }
+    const countReset = () => {
+      setCount(0);
+      console.log(count);
+    }
+
+render()
   return (
-    <div className="App">
-      <button onClick={buttonAlert}>Hello World</button>
+    <div>
+      <div>
+        <button onClick={increment}>いいね</button>
+        <button onClick={decrement}>よくないね</button>
+        <button onClick={countReset}>リセット!</button>
+        {count}
+      </div>
+      <div>
+        <h1>Hello world</h1>
+        <h2>Hello world</h2>
+        <h3>Hello world</h3>
+        <h4>Hello world</h4>
+
+        <button
+          onClick={() => window.alert("Hello world")}
+        ></button>
+
+        <button>button</button>
+        <input type="text" />
+        <a href='#'>a タグ</a>
+        {name}
+      </div>
+      <div className="App">
+        <button onClick={buttonAlert}>Hello World</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
