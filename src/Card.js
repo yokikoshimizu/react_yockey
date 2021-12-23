596812a1099ed85c6da30a5e3ef40298d4d77082
yokikoshimizu <heppoko.yokiki@gmail.com>
2021-12-23 11:48:40 +0900

    .

--- src/Card.js
+++ src/Card.js
@@ -5,9 +5,18 @@ const Card = (props) => {
     console.log(props)
     return(
         <div className="wrapper">
-            <h1>{props.id}</h1>
-            <h1>{props.name}</h1>
-            <h1>{props.date}</h1>
+            <table>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>date</th>
+                </tr>
+                <tr>
+                    <td><h1>{props.id}</h1></td> 
+                    <td><h1>{props.name}</h1></td>
+                    <td><h1>{props.date}</h1></td>
+                </tr>
+            </table>
         </div>
     );
 };
