63e1b8fe14df1159fc7117a25bbacf6831869c35
yokikoshimizu <heppoko.yokiki@gmail.com>
2022-01-22 23:28:42 +0900

    .

--- src/components/ComponentB.js
+++ src/components/ComponentB.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
 
 const ComponentB = () => {
+  const [data,setData] = useState([]);
+    console.log('20220122');
+  
+
+  useEffect(() => {
+    console.log('useEffect が呼び出されました。');
+
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+        console.log(res.data, 'res check');
+        setData(res.data)
+  })
+
+}, []);
+
   return (
-    <div>
-      <div>ComponentB</div>
-      <Link to="componentc">ComponentCへ移動</Link>
-    </div>
+    <>
+      <div>
+        <div>ComponentB</div>
+        <Link to="componentc">ComponentCへ移動</Link>
+      </div>
+      <Table striped bordered hover>  
+      <thead>
+        <tr>
+          <th>
+            userID
+          </th>
+          <th>
+            id
+          </th>
+          <th>
+            title
+          </th>
+          <th>
+            body
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+            { data.map((d,index) => {
+                return (
+                  <tr key={index}>
+                      <td>{d.userID}</td>
+                      <td>{d.id}</td>
+                      <td>{d.title}</td>
+                      <td>{d.body}</td>
+                  </tr>
+                  )
+              })}
+      </tbody>
+  </Table>
+  </>
   );
 };
 
--- src/components/ComponentC.js
+++ src/components/ComponentC.js
@@ -1,12 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 const ComponentC = () => {
+  const [data,setData] = useState([]);
+    console.log('20220122');
+  
+
+  useEffect(() => {
+    console.log('useEffect が呼び出されました。');
+
+    axios.get('https://jsonplaceholder.typicode.com/comments').then(res => {
+        console.log(res.data, 'res check');
+        setData(res.data)
+  })
+
+}, []);
+
   return (
-    <div>
-      <div>ComponentC</div>
-      <Link to="/">ComponentAへ移動</Link>
-    </div>
+    <>
+      <div>
+        <div>ComponentC</div>
+        <Link to="/">ComponentAへ移動</Link>
+      </div>
+      <Button type="submit" onClick={() =>
+        <Table striped bordered hover>  
+          <thead>
+            <tr>
+              <th>
+                postID
+              </th>
+              <th>
+                id
+              </th>
+              <th>
+                name
+              </th>
+              <th>
+                email
+              </th>
+              <th>
+                body
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+                { data.map((d,index) => {
+                    return (
+                      <tr key={index}>
+                          <td>{d.pstID}</td>
+                          <td>{d.id}</td>
+                          <td>{d.name}</td>
+                          <td>{d.email}</td>
+                          <td>{d.body}</td>
+                      </tr>
+                      )
+                  })}
+          </tbody>
+        </Table>
+      }>
+      GetData
+      </Button>
+    </>
   );
 };
 
